ccc762be37b2e984ded9d9f7c00f07a61794a2ed
amitauti <amitauti@users.noreply.github.com>
2021-08-10 11:17:19 +0530

    Create lambda.js

---
+++ lambda.js
@@ -0,0 +1,68 @@
+const AWS = require("aws-sdk");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "DELETE /items/{id}":
+        await dynamo
+          .delete({
+            TableName: "http-crud-tutorial-items",
+            Key: {
+              nightId: event.pathParameters.id
+            }
+          })
+          .promise();
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+      case "GET /items/{id}":
+        body = await dynamo
+          .get({
+            TableName: "http-crud-tutorial-items",
+            Key: {
+              nightId: event.pathParameters.id
+            }
+          })
+          .promise();
+        break;
+      case "GET /items":
+        body = await dynamo.scan({ TableName: "http-crud-tutorial-items" }).promise();
+        break;
+      case "PUT /items":
+        let requestJSON = JSON.parse(event.body);
+        await dynamo
+          .put({
+            TableName: "http-crud-tutorial-items",
+            Item: {
+              nightId: requestJSON.nightId,
+              startTimeMilli: requestJSON.startTimeMilli,
+              endTimeMilli: requestJSON.endTimeMilli,
+              sleepQuality: requestJSON.sleepQuality
+            }
+          })
+          .promise();
+        body = `Put item ${requestJSON.nightId}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+};
